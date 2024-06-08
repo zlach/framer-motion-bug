cb30f34171a653adcfac4d4d5dd3d532e685948e
Zach Goldberg <zachgoldberg@Zachs-MacBook-Pro.local>
2024-06-08 18:25:53 -0500

    increased height

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,16 +5,22 @@ export default function Index() {
   const [items, setItems] = useState([0, 1, 2, 3]);
 
   return (
-    <Reorder.Group axis="y" values={items} onReorder={setItems}>
-      {items.map((item) => (
-        <Reorder.Item key={item} value={item}>
-          <div
-            style={{ border: "1px solid blue", width: "200px", height: "50px" }}
-          >
-            {item}
-          </div>
-        </Reorder.Item>
-      ))}
-    </Reorder.Group>
+    <div style={{ height: "1000px" }}>
+      <Reorder.Group axis="y" values={items} onReorder={setItems}>
+        {items.map((item) => (
+          <Reorder.Item key={item} value={item}>
+            <div
+              style={{
+                border: "1px solid blue",
+                width: "200px",
+                height: "50px",
+              }}
+            >
+              {item}
+            </div>
+          </Reorder.Item>
+        ))}
+      </Reorder.Group>
+    </div>
   );
 }
